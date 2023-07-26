5c8b3de2da6bd461431e5f91b1c400e7e0386b2c
Mac-ziis <macg286@gmail.com>
2023-07-25 17:29:50 -0700

    Configures the webpack for images

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,26 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      
     ]
   }
 };
